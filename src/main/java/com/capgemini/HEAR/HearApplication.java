96e44a8fed6bfdcb2e5e166b534a7711683c3a11
RobinB87 <38104646+RobinB87@users.noreply.github.com>
2018-04-24 14:13:10 +0200

    Nieuwe bootstrap file

---
+++ src/main/java/com/capgemini/HEAR/HearApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.HEAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearApplication.class, args);
+	}
+}
