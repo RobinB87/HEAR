9a6f5c79582339c65630cb6c02d4ced58a741e1e
ashjaminhas <38127720+ashjaminhas@users.noreply.github.com>
2018-05-03 17:17:16 +0200

    Overige buttons aangemaakt.

--- src/main/resources/static/admin/js/adminLayout.js
+++ src/main/resources/static/admin/js/adminLayout.js
@@ -19,7 +19,48 @@ $('#viewCategory').click(function(e) {
 e.preventDefault();
     $.get('category/index.html', function(data) {
         $('#content-box-hear').html(data);
+     });
+});
 
-        });
+$('#createSubcategory').click(function(e) {
+e.preventDefault();
+   $.get('subcategory/create.html', function(data) {
+       $('#content-box-hear').html(data);
+   });
+});
 
-        });
+$('#viewSubcategory').click(function(e) {
+e.preventDefault();
+    $.get('subcategory/index.html', function(data) {
+        $('#content-box-hear').html(data);
+    });
+});
+
+$('#createMenuItem').click(function(e) {
+e.preventDefault();
+   $.get('menuitem/create.html', function(data) {
+       $('#content-box-hear').html(data);
+   });
+});
+
+$('#viewMenuItem').click(function(e) {
+e.preventDefault();
+   $.get('menuitem/index.html', function(data) {
+       $('#content-box-hear').html(data);
+   });
+});
+
+$('#createIngredient').click(function(e) {
+e.preventDefault();
+   $.get('ingredients/create.html', function(data) {
+       $('#content-box-hear').html(data);
+   });
+});
+
+$('#viewIngredient').click(function(e) {
+e.preventDefault();
+   $.get('ingredients/index.html', function(data) {
+       $('#content-box-hear').html(data);
+   });
+});
+
