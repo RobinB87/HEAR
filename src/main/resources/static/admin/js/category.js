3f9aab61043af6d1de2ef19c39f7620f76b7e348
Emile Verbunt <emileverbunt@gmail.com>
2018-05-04 13:50:48 +0200

    * Fixed dataTables for category to with with AJAX AMSTERDAM!

--- src/main/resources/static/admin/js/category.js
+++ src/main/resources/static/admin/js/category.js
@@ -1,4 +1,20 @@
-$("#addCategoryBtn").click(function(e) {
+// DataTable for categories
+$(document).ready(function () {
+    $("#categoryTable").DataTable({
+        "processing": true,
+        "serverSide": true,
+        'ajax': {
+            'url': '/api/category/all',
+            'dataSrc': ''
+        },
+        'columns': [
+            {"data": "id"},
+            {"data": "title"}
+        ]
+    });
+});
+
+$("#addCategoryBtn").click(function (e) {
     e.preventDefault();
 
     var title = $('#titleField').val();
