f27ca58902a1f1c9dc051ac6b59c3e8f94dcb1b9
Emile <emileverbunt@gmail.com>
2018-05-06 23:10:45 +0200

    * select2 setup

--- src/main/resources/static/admin/js/subcategory.js
+++ src/main/resources/static/admin/js/subcategory.js
@@ -1,4 +1,26 @@
-$("#addSubCategoryBtn").click(function(e) {
+$(document).ready(function () {
+    var categoryList = $.get('/api/category/all', function (data) {
+        return data;
+    });
+
+    $('#categoryListSelect2').select2({
+        data: {
+            results: function(data) {
+                return {
+                    results: $.map(data, function(item) {
+                        return {
+                            text: item.title,
+                            id: item.id
+                        }
+                    })
+                }
+            }
+        }
+    });
+});
+
+
+$("#addSubCategoryBtn").click(function (e) {
     e.preventDefault();
 
     var title = $('#titleField').val();
