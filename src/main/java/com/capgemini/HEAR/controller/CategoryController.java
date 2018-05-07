da4c99aca06aeae5b597e8a5c2ce97aff9ed9f43
Emile Verbunt <emileverbunt@gmail.com>
2018-05-07 10:46:46 +0200

    * Used SELECT2 * added jquery scripts

--- src/main/java/com/capgemini/HEAR/controller/CategoryController.java
+++ src/main/java/com/capgemini/HEAR/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.capgemini.HEAR.repository.ICategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
--- src/main/resources/static/admin/js/subcategory.js
+++ src/main/resources/static/admin/js/subcategory.js
@@ -1,22 +1,33 @@
-$(document).ready(function () {
-    var categoryList = $.get('/api/category/all', function (data) {
-        return data;
-    });
+var categoryArray = [];
 
-    $('#categoryListSelect2').select2({
-        data: {
-            results: function(data) {
-                return {
-                    results: $.map(data, function(item) {
-                        return {
-                            text: item.title,
-                            id: item.id
-                        }
-                    })
-                }
-            }
-        }
-    });
+$.ajax({
+    type: 'GET',
+    url: '/api/category/all',
+    dataType: 'json',
+    success: function (data) {
+        $.each(data, function (i, itemData) {
+            categoryArray[i] = itemData;
+        });
+    }
+});
+
+$('#newSubcategoryModal').click(function() {
+    for (var i = 0; i < categoryArray.length; i++) {
+        $('#categoryListSelect2')
+            .append($("<option></option>")
+                .attr("value", categoryArray[i].id)
+                .text(categoryArray[i].title));
+    }
+});
+
+$('#closeButton').click(function() {
+   for(var i = 0; i < categoryArray.length; i++) {
+       $('#categoryListSelect2').empty();
+   }
+});
+
+$(document).ready(function() {
+    $('#categoryListSelect2').select2();
 });
 
 
