e01ad2ee42bac8fe6593ac573cfd30c1d854b91b
Harald <37929926+harald1992@users.noreply.github.com>
2018-05-07 15:44:06 +0200

    Delete button

--- src/main/resources/static/admin/js/category.js
+++ src/main/resources/static/admin/js/category.js
@@ -11,16 +11,29 @@ $(document).ready(function () {
             {"data": "id"},
             {"data": "title"},
             {
+                "targets": -2,
+                "data": null,
+                "defaultContent": "<button class='btn btn-default editBtn'>Edit</button>"
+            },
+            {
                 "targets": -1,
                 "data": null,
-                "defaultContent": "<button class='btn btn-default'>Edit</button>"
-            }]
+                "defaultContent": "<button class='btn btn-default deleteBtn'>Delete</button>"
+            }
+            ]
     });
 
-    $('#categoryTable tbody').on( 'click', 'button', function () {
+    $('#categoryTable tbody').on( 'click', '.editBtn', function () {
         var data = table.row( $(this).parents('tr') ).data();
         alert('You want to edit: ' + data.title + ' with id: ' + data.id );
     } );
+
+    $('#categoryTable tbody').on( 'click', '.deleteBtn', function() {
+        var data = table.row( $(this).parents('tr') ).data();
+        alert('Delete Button: ' + data.title + ' with id: ' + data.id );
+
+
+    })
 });
 
 $("#addCategoryBtn").click(function (e) {
