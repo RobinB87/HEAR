79970314e823c40c9d96786e6b145fe946018b49
ashjaminhas <38127720+ashjaminhas@users.noreply.github.com>
2018-05-09 16:54:33 +0200

    * Merge errors gefixt

--- src/main/resources/static/admin/js/category.js
+++ src/main/resources/static/admin/js/category.js
@@ -37,13 +37,8 @@ $(document).ready(function () {
         $('#categoryIdField').val(data.id);
         $('#editCategoryTitleField').val(data.title);
 
-<<<<<<< HEAD
         $('#editCategoryEditBtn').click(function () {
-=======
-        // gebruik modal() om modal te openen
 
-        $('#editCategoryBtn').click(function () {
->>>>>>> 683d28ca507f6db2bdc6888b70cb935a0174d28e
             var title = $('#editCategoryTitleField').val();
             $.post('/api/category/edit', {
                 id: data.id,
@@ -61,11 +56,7 @@ $(document).ready(function () {
 
         // get the values of this Id category:
         $('#categoryIdField').val(data.id);         //get id
-<<<<<<< HEAD
 		$('#deleteCategoryTitleField').val(data.title);
-=======
-        // gebruik modal() om modal te openen
->>>>>>> 683d28ca507f6db2bdc6888b70cb935a0174d28e
 
         $('#deleteCategoryConfirmBtn').click(function () {           //button function
             $.get('/api/category/delete/' + data.id, {
@@ -74,12 +65,6 @@ $(document).ready(function () {
                 table.clear().draw();
             });
         });
-<<<<<<< HEAD
-});
-
-});
-
-=======
     });
 
     $("#addCategoryBtn").click(function (e) {
@@ -96,6 +81,4 @@ $(document).ready(function () {
 
         console.log("plek 2");
     });
-
 });
->>>>>>> 683d28ca507f6db2bdc6888b70cb935a0174d28e
