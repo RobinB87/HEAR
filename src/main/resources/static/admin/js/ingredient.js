dd9288c03e35c3ea264a4852b6a1ce7b78da05e7
ashjaminhas <38127720+ashjaminhas@users.noreply.github.com>
2018-05-14 12:45:36 +0200

    Fixed Ingredients

--- src/main/resources/static/admin/js/ingredient.js
+++ src/main/resources/static/admin/js/ingredient.js
@@ -1,4 +1,26 @@
-$("#addIngredientBtn").click(function (e) {
+$(document).ready(function () {
+        var table = $("#ingredientTable").DataTable({
+            'ajax': {
+                'url': '/api/ingredient/all',
+                'dataSrc': ''
+            },
+            "columns": [
+                {"data": "id"},
+                {"data": "title"},
+                {"data": "price"},
+
+                {
+                    "targets": -1,
+                    "data": null,
+                    "defaultContent": "<i class='fa fa-edit editBtn'></i> | <i class='fa fa-trash deleteBtn'></i>"
+                }
+                ]
+        });
+});
+
+
+
+$("#addIngredientSubmitBtn").click(function (e) {
     e.preventDefault();
 
     var title = $('#ingredientTitleField').val();
@@ -7,5 +29,7 @@ $("#addIngredientBtn").click(function (e) {
     $.post('/api/ingredient/add', {
         title: title,
         price: price
-    });
+    },  function() {
+             table.clear().draw();
+                })
 });
